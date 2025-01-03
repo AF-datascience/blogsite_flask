6e2f1efad4f57ac7e599b5264365426079c48f77
AF-datascience <afazil1994@gmail.com>
2025-01-03 15:41:30 +0000

    updated the templates for the microblog

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,9 @@
 # to execture whena cleint requests a given URL. 
 
 # The view function here just returns a greeting for these two urls
-
+from flask import render_template
 from app import app 
+
 
 # decorators here will be modify the function of index()
 # this will be to register a function as a callback for a certain event 
@@ -14,16 +15,25 @@ from app import app
 # so when the browser requests / or index urls flask will invoke this function 
 # and pass its return value back to the browser as a response. 
 
+# Templates will be used in place of presentation of the html 
+# allowing us to focus solely on the python code.
+
 @app.route("/") 
 @app.route("/index")
 def index(): 
     user = {'username': 'Random User 1'}
-    return '''
-<html> 
-    <head> 
-        <title> Home Page - Microblog Site </title>
-    </head>
-    <body>
-        <h1> Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+
+    # mock example to show users information looping: 
+    posts = [
+        {'author': {'username':'John'}, 
+         'body': 'Beautiful day in London'}, 
+        {'author': {'username': 'Jane'}, 
+         'body': "Weather is really nice too"},
+        {'author': {'username':'Bobby'}, 
+         'body': 'get outta here'}, 
+        {'author': {'username': 'james'}, 
+         'body': "new york is way better"}
+    ]
+
+
+    return render_template('index.html', title = '', user = user, posts = posts)
