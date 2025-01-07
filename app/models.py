ce6773d42efcfeaf1f9254c2e52b3e9b8a87cd4b
AF-datascience <afazil1994@gmail.com>
2025-01-07 11:09:29 +0000

    updated flask blog to include user logins

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@
 #user class represents a users stored int eh database
 from typing import Optional 
 from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db 
@@ -15,6 +16,17 @@ class User(db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates = 'author')
+
+
+    def set_password(self, password): 
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password) 
+
+
+
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
